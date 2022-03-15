ec87c2dc0bbaa16e5522670568c448448b1949c5
juzer bhagat <juzerbhagat25@gmail.com>
2022-03-15 14:19:01 -0400

    Signup page completed

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
-import { makeStyles, TextField, Button, Radio, RadioGroup } from '@material-ui/core';
-import { FormControlLabel, FormLabel } from "@material-ui/core";
+//import { makeStyles, TextField, Button, Radio, RadioGroup } from '@material-ui/core';
+//import { FormControlLabel, FormLabel } from "@material-ui/core";
+import {Form, FloatingLabel, Button, Row, Col}from 'react-bootstrap'
 import '../App.css';
 
-const useStyles = makeStyles(theme => ({
+/*const useStyles = makeStyles(theme => ({
     root: {
       display: 'flex',
       flexDirection: 'column',
@@ -22,60 +23,106 @@ const useStyles = makeStyles(theme => ({
       
     },
   }));
-  
+  */
 
 const Signup = (props) => {
-    const classes = useStyles();
+    //const classes = useStyles();
     return (
-        <div>
-            <form className={classes.root}>
-                <TextField 
-                    label='First Name'
-                    variant='filled'
+        <div className="App-body">
+            <Form className='signupForm'>
+               <h2>Signup Form</h2>
+               <Row className="mb-4">
+               <Form.Group as={Col}>
+               <FloatingLabel
+                controlId="firstName"
+                label="First Name"
+                className="mb-3"
+                >
+                    <Form.Control type="text" placeholder="First Name"
+                        className="textform"
+                    />
+                </FloatingLabel>
+                </Form.Group>
+                <Form.Group as={Col}>
+                    <FloatingLabel
+                        controlId="lastName"
+                        label="Last Name"
+                        className="mb-3"
+                    >
+                    <Form.Control type="text" placeholder="Last Name"
+                        className="textform"
+                    />
+                    </FloatingLabel>
+                </Form.Group>
+                </Row>
+  
+                <Form.Group>
+                    <FloatingLabel
+                        controlId="email"
+                        label="Email"
+                        className="mb-3"
+                    >
+                    <Form.Control type="email" placeholder="name@example.com"
+                        className="textform"
+                    />
+                </FloatingLabel>
+  
+  
+                <FloatingLabel controlId="floatingPassword" label="Password" className="mb-3">
+                    <Form.Control type="password" placeholder="Password" 
+                        className="textform"
+                    />
+                </FloatingLabel>
+
+                <FloatingLabel controlId="floatingCPassword" label="Confirm Password" className="mb-3">
+                    <Form.Control type="password" placeholder="Confirm Password"
+                        className="textform"
+                    />
+                </FloatingLabel>
+
+                <FloatingLabel controlId="date" label="Date of Birth" className="mb-3">
+                    <Form.Control type="date" placeholder="Date of Birth" 
+                        className="textform"
+                    />
+                </FloatingLabel>
+
+                <Form.Label className="select" >
+                Gender
+                <Form.Select aria-label="Gender" size='lg' className="textform">
+                    <option></option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="others">Others</option>
+                    <option value="nodisclosure">Prefer not to disclose</option>
+                </Form.Select>
+                </Form.Label>
+
+                <Form.Label  className="select">
+                Relationship Status
+                <Form.Select aria-label="relationship status" className="textform" size='lg'>
+                <option></option>
+                <option value="married">Married</option>
+                <option value="single">Single</option>
+                <option value="inarelation">In a Relationship</option>
+                <option value="nodisclosure">Prefer not to disclose</option>
+                </Form.Select>
+                </Form.Label>
+
+
+                <FloatingLabel controlId="floatingtextarea" label="Interests" className="mb-3">
+                    <Form.Control as="textarea" rows={10} placeholder="Interests"
+                    className="textarea"
                 />
+                </FloatingLabel>
 
-                <TextField 
-                    label='Last Name'
-                    variant='filled'
-                />
-
-                <TextField 
-                    label='email'
-                    variant='filled'
-                /> 
-
-                <TextField 
-                    label='password'
-                    variant='filled'
-                    type='password'
-                />
-
-                <TextField 
-                    label='confirm password'
-                    variant='filled'
-                    type='password'
-                />
-                   
-                <TextField 
-                    
-                    variant='filled'
-                    type='date'
-                />
-                <FormLabel id='demo-radio-buttons-group-label'>Gender</FormLabel>
-                <RadioGroup
-                aria-labelledby="demo-radio-buttons-group-label"
-                defaultValue=""
-                
-                name="radio-buttons-group">
-                <FormControlLabel value="female" control={<Radio />} label="Female" />
-                <FormControlLabel value="male" control={<Radio />} label="Male" />
-                <FormControlLabel value="other" control={<Radio />} label="Other" />
-                </RadioGroup>
-
-
-
-
-            </form>
+                </Form.Group>
+ 
+  
+                <Button variant="primary" type="submit" className="submit">
+    Submit
+                </Button>
+            </Form> 
+        
         </div>
     );
 };
