8e1055592ace2c8f73faacd366c4988008f7ff6a
juzer bhagat <juzerbhagat25@gmail.com>
2022-03-19 19:50:02 -0400

    Completed News page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Signup from './components/Signup';
 import Login from './components/Login';
+import News from './components/News';
 import './App.css';
 import {BrowserRouter as Router, Routes ,Route, Link} from 'react-router-dom';
 
@@ -15,7 +16,7 @@ function App() {
           <h1>Welcome to DuckBook</h1>
           <Link className='App-link' to='signup'>Signup</Link>
           <Link className='App-link' to='login'>Login</Link>
-          
+          <Link className='App-link' to='news'>News</Link>
         </header>
         
         <div className='App-body'>
@@ -23,6 +24,8 @@ function App() {
             
             <Route  path = '/signup' element={<Signup />}/>
             <Route  path = '/login' element={<Login />}/>
+
+            <Route path= '/news' element={<News />} />
           </Routes>
         </div>
        
