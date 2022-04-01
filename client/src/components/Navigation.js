8ae8aff511f89c48becbb8cbb1bb5b7df9bf04fa
“Akshay” <asahasra@stevens.edu>
2022-04-01 17:29:58 -0400

    navigation styling

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,56 +1,68 @@
 import React, { useState, useEffect } from "react";
 import { Form } from "react-bootstrap";
 import '../App.css';
+import logoImg from '../images/logo.gif';
 
 const Navigation = (props) => {
     return (
         // ---------- Start of Navigation ---------- // 
        <section className="section nav-section fixed-top facebook-light-gray-color">
             <nav className="navbar navbar-expand-lg horizontal-navigation navbar-expand-lg navbar-light facebook-light-gray-color">
-                <a class="navbar-brand" href="#"><img src="public/logo.gif" alt="img"/></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <button class="btn btn-outline-primary" type="submit">Search</button>
-                </form>
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link mobile-nav-link" aria-current="page" href="#">Akshay</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link mobile-nav-link" aria-current="page" href="#">Friends</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link mobile-nav-link" aria-current="page" href="#">Videos</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link mobile-nav-link" aria-current="page" href="#">Memories</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link mobile-nav-link" aria-current="page" href="#">News</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link mobile-nav-link" aria-current="page" href="#">Weather</a>
-                    </li>
-                    {/* <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
+                    <a className="navbar-brand logo-link me-3 ms-3" href="#">
+                    <img class="responsive-img logo-img" src={logoImg} alt="img" />
                     </a>
-                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a className="dropdown-item" href="#">Action</a></li>
-                        <li><a className="dropdown-item" href="#">Another action</a></li>
-                        <li><hr className="dropdown-divider"></hr></li>
-                        <li><a className="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                    </li> */}
-                </ul>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                <div className="collapse navbar-collapse d-flex justify-content-between" id="navbarSupportedContent">
+                    <div>
+                        <form class="d-flex">
+                            <input class="form-control search-textbox" type="search" placeholder="Search" aria-label="Search"/>
+                            {/* <button class="btn btn-outline-primary" type="submit">Search</button> */}
+                        </form>
+                    </div>
+                    <div>
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <span class="material-icons home-icon">home</span>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link mobile-nav-link" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link mobile-nav-link" aria-current="page" href="#">Akshay</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link mobile-nav-link" aria-current="page" href="#">Friends</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link mobile-nav-link" aria-current="page" href="#">Videos</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link mobile-nav-link" aria-current="page" href="#">Memories</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link mobile-nav-link" aria-current="page" href="#">News</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link mobile-nav-link" aria-current="page" href="#">Weather</a>
+                            </li>
+                            {/* <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a className="dropdown-item" href="#">Action</a></li>
+                                <li><a className="dropdown-item" href="#">Another action</a></li>
+                                <li><hr className="dropdown-divider"></hr></li>
+                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                            </li> */}
+                        </ul>
+                    </div>
+                    <div>
+                        <button class="btn btn-outline-primary me-3 ms-3" type="submit">Logout</button>
+                    </div>
                 
                 </div>
             </nav>
