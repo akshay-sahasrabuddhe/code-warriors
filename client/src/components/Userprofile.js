1ae6dbf6d3f7462d558bcde2a98d5dcfdff13641
“Akshay” <asahasra@stevens.edu>
2022-04-19 00:41:41 -0400

    about section is done

--- client/src/components/Userprofile.js
+++ client/src/components/Userprofile.js
@@ -12,15 +12,98 @@ import { Modal } from 'bootstrap';
 const Userprofile = (props) => {
 
     return (
-        // ---------- Start of Posts ---------- // 
         <> 
         <Navigation></Navigation>  
-        <section className="posts-section">
+        {/* // ---------- Start of User Profile Section ---------- //  */}
+        <section className="user-profile-section">
             <div className="user-profile-box">
-                <img src={maleUser} class="user-profile-pic" alt="User Profile Pic" />
+                <img src={maleUser} className="user-profile-pic" alt="User Profile Pic" />
+            </div>
+        </section>
+        {/* // ---------- End of User Profile Section ---------- //  */}
+
+        {/* // ---------- Start of User profiel Tabs Section ---------- //  */}
+
+        <section className="user-profiel-tabs-section">
+            <ul className="nav nav-tabs justify-content-center" id="myTab" role="tablist">
+                <li className="nav-item" role="presentation">
+                    <button className="nav-link w-100 active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">About</button>
+                </li>
+                <li className="nav-item" role="presentation">
+                    <button className="nav-link w-100" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Friends</button>
+                </li>
+                <li className="nav-item" role="presentation">
+                    <button className="nav-link w-100" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false">Edit Profile</button>
+                </li>
+            </ul>
+            <div className="tab-content border border-top-0" id="myTabContent">
+                <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                    <div className="row">
+                        <div className="col-md-5 offset-2 offset-md-1">
+                            <div className="d-flex  pt-4 pb-4">
+                                <div className="d-flex align-self-center">
+                                    <span class="material-icons-outlined about-box-img">email</span>
+                                </div>
+                                <div className="d-flex flex-column m-2">
+                                    <span className="about-heading">asahasra@stevens.edu</span>
+                                    <span className="about-subheading text-secondary">Email</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-5 offset-2 offset-md-1">
+                            <div className="d-flex  pt-4 pb-4">
+                                <div className="d-flex align-self-center">
+                                    <span className="material-icons-outlined about-box-img">cake</span>
+                                </div>
+                                <div className="d-flex flex-column m-2">
+                                    <span className="about-heading">1 November 1996</span>
+                                    <span className="about-subheading text-secondary">Birthday</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-5 offset-2 offset-md-1">
+                            <div className="d-flex  pt-4 pb-4">
+                                <div className="d-flex align-self-center">
+                                    <span className="material-icons-outlined about-box-img">boy</span>
+                                    {/* <span className="material-icons-outlined about-box-img">girl</span> */}
+                                    {/* <span className="material-icons-outlined about-box-img">transgender</span> */}
+                                </div>
+                                <div className="d-flex flex-column m-2">
+                                    <span className="about-heading">Man</span>
+                                    <span className="about-subheading text-secondary">Gender</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-5 offset-2 offset-md-1">
+                            <div className="d-flex  pt-4 pb-4">
+                                <div className="d-flex align-self-center">
+                                    <span class="material-icons-outlined about-box-img">favorite</span>
+                                </div>
+                                <div className="d-flex flex-column m-2">
+                                    <span className="about-heading">Single</span>
+                                    <span className="about-subheading text-secondary">Relationship Status</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-10 offset-2 offset-md-1">
+                            <div className="pt-4 pb-4 d-flex align-self-center">
+                                <div className="d-flex align-self-center">
+                                    <span className="material-icons-outlined about-box-img">interests</span>
+                                </div>
+                                <div className="d-flex flex-column m-2">
+                                    <span className="about-heading">Cricket, Football, Singing</span>
+                                    <span className="about-subheading text-secondary">Interests</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+                <div className="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
             </div>
         </section>
 
+        {/* // ---------- End of User profiel Tabs Section ---------- //  */}
         </>
         
     );
