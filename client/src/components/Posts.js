9bc532b3ec5219c5b0acae36c326cf628bea2596
“Akshay” <asahasra@stevens.edu>
2022-05-10 02:39:53 -0400

    changes

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -181,6 +181,33 @@ const Posts = (props) => {
 
             // 
 
+            async function openeditmodal(){
+           
+                const instance = axios.create({
+                    baseURL: '*',
+                    timeout: 20000,
+                    withCredentials: true,
+                headers: {
+                    'Content-Type': 'application/json;charset=UTF-8',
+                    "Access-Control-Allow-Origin": "*",
+                    },
+                validateStatus: function (status) {
+                    return status < 500; // Resolve only if the status code is less than 500
+                    }
+                });
+                const userdata1  = await instance.get(`http://localhost:3000/session`);
+                console.log(n.userThatPosted._id);
+                console.log(userdata1.data.id);
+                if(userdata1.data.id === n.userThatPosted._id){
+                let myEditModal = new Modal(document.getElementById('editModal'));
+                myEditModal.show();
+                }else{
+                    alert("You cannot edit this post.");
+                }
+            }
+            
+    
+
 
             async function deletepost(delid){
                 delid = editdataid
@@ -252,7 +279,162 @@ const Posts = (props) => {
                     </div>
                 </div>
             </div>
-        </div>
+                </div>
+                <div className="modal fade" id="editModal" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex={-1} aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">Modal title</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                            <form onSubmit={
+    async (e)=>{
+        e.preventDefault();
+       let btn = document.getElementById("sub1");
+      btn.disabled = false;
+    //    if(!title.value){
+    //        console.log(title.value);
+    //        alert("Please enter post title");
+    //        btn.disabled = false;
+    //        return;
+    //    }
+    //    if(title.value.trim().length == 0){
+    //     alert("Only white spaces are not allowed.");
+    //     btn.disabled = false;
+    //        return;
+    //    }
+
+    //    let regf = /^[ A-Za-z0-9_@./#&+-]*$/
+    //    if(!regf.test(title.value)){
+    //         alert("Please enter valid post title");
+    //         btn.disabled = false;
+    //         return;
+    //    }
+
+    //    if(!body.value){
+    //        alert("Please enter first post description");
+    //        btn.disabled = false;
+    //        return;
+    //    }
+    //    if(body.value.trim().length == 0){
+    //     alert("Only white spaces are not allowed.");
+    //     btn.disabled = false;
+    //        return;
+    //    }
+
+    //    if(!regf.test(body.value)){
+    //         alert("Please enter valid post description");
+    //         btn.disabled = false;
+    //         return;
+    //    }
+      
+       // let temp = JSON.parse(user);
+       let flag = false;
+        //let msg = await axios.post('http:/localhost:3000/signup', user);
+
+        const instance = axios.create({
+            baseURL: '*',
+            timeout: 20000,
+            withCredentials: true,
+        headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+            "Access-Control-Allow-Origin": "*",
+            },
+        validateStatus: function (status) {
+            return status < 500; // Resolve only if the status code is less than 500
+            }
+        });
+        
+        
+
+        let postid = n._id;
+
+        const clickedpost = await instance.get(`http://localhost:3000/posts/${postid}`);
+        
+    
+               
+        n.title = clickedpost.data.title;
+        n.body = clickedpost.data.body;
+
+       
+        const data1  = await instance.get(`http://localhost:3000/session`);
+                console.log(data1.data);
+            
+        const userdata1  = await instance.get(`http://localhost:3000/getUserData`);
+        console.log(userdata1.data);
+
+        
+        let user = {
+                        id: postid,
+                        title : document.getElementById('title').value,
+                        body:  document.getElementById('body').value,
+                        isPublic: true,
+                    }
+                    console.log(user);
+        
+        // let formData1 = new FormData();
+        //         formData1.append("id",postid);
+        //         formData1.append("title",title.value);
+        //         formData1.append("body" , body.value);
+        //         formData1.append("isPublic", true);
+               
+                
+        // console.log("yE HAI AASLI"+formData1.get('title'));
+
+
+
+        await axios.patch(`http://localhost:3000/posts/${postid}`,user,{
+            headers: {
+                  'Content-Type': 'application/json;charset=UTF-8',
+                  "Access-Control-Allow-Origin": "*",
+                }
+                }).then(function (response) {
+            console.log(response.data);
+
+            if(response.status === 200){
+                alert("Post posted successfully!!!");
+                flag = true;
+                window.location.reload();
+            }
+          }).catch(function (error) {
+            console.log(error);
+            //setSuccess(false);
+            alert("There was some error please try again");
+
+          });
+
+          e.target.reset();
+          //btn.disabled = false;
+           if(flag){
+            
+            props.handleClose(false);
+           }            
+    }
+}>
+
+                        <div className="mb-3">
+                            <label htmlFor="title" className="form-label">Title</label>
+                            <input type="text" className="form-control" id="title" aria-describedby="emailHelp" ref={(node)=>{
+                            title = node;
+                        }} />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="body" className="col-form-label">Message:</label>
+                            <textarea className="form-control" id="body" defaultValue={n.body} ref={(node)=>{
+                            body = node;
+                        }} ></textarea>
+                        </div>
+                        <div className="mb-3">
+                            <button type="button" className="btn btn-secondary me-3" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" className="btn btn-primary" id="sub1">Publish</button>
+                        </div>
+                        </form>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
            
             <div className="card post-card align-self-center mb-3">
             <div className="post-row justify-content-between">
@@ -270,12 +452,21 @@ const Posts = (props) => {
                         </div>
                     </div>
                 </div>
+                <div className="d-flex justify-content-between">
+                <div className="p-2 p-lg-3 pb-0 pb-lg-0 ps-lg-0">
+                        <button type="button" className="btn btn-outline-primary delete-post-btn d-flex" onClick={() => openeditmodal(n._id)}>
+                            <span className="material-icons">
+                                edit
+                            </span>
+                        </button>
+                    </div>
                 <div className="p-2 p-lg-3 pb-0 pb-lg-0 ps-lg-0">
                         <button type="button" className="btn btn-outline-danger delete-post-btn d-flex" onClick={() => opendeltemodal(n._id)}>
                             <span className="material-icons">
                                 delete
                             </span>
                         </button>
+                    </div>
                     </div>
             </div>
             <hr className="m-0 mb-2"></hr>
